70916d0cf0fb44f80a588338219cc9271102e5e2
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-11-10 23:44:46 -0300

    feat(components): Adiciona componente photos-search

---
+++ src/components/photos-search.tsx
@@ -0,0 +1,34 @@
+import InputText from "./input-text";
+import SearchIcon from "../assets/icons/search.svg?react";
+import React from "react";
+import { debounce } from "../helpers/utils";
+import usePhotos from "../context/photos/hooks/use-photos";
+
+export default function PhotosSearch() {
+  const [inputValue, setInputValue] = React.useState("");
+  const { filters } = usePhotos();
+
+  const debouncedSetValue = React.useCallback(
+    debounce((value: string) => {
+      filters.setQ(value);
+    }, 200),
+    [filters.setQ]
+  );
+
+  function heandleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+
+    setInputValue(value);
+    debouncedSetValue(value);
+  }
+
+  return (
+    <InputText
+      icon={SearchIcon}
+      placeholder="Buscar fotos"
+      className="flex-1"
+      value={inputValue}
+      onChange={heandleInputChange}
+    />
+  );
+}
