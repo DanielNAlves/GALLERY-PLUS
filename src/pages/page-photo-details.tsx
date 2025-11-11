dda6b54641d85caa03ae4bae7fb2b5f9c9aa17db
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-11-10 23:52:48 -0300

    feat(pages): Adiciona página de detalhes da foto

---
+++ src/pages/page-photo-details.tsx
@@ -0,0 +1,72 @@
+import { useParams } from "react-router";
+import Text from "../components/text";
+import Container from "../components/container";
+import Skeleton from "../components/skeleton";
+import PhotoNavigator from "../context/photos/components/photo-navigator";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
+import AlbumsListSelectable from "../context/albums/components/albums-list-selectable";
+import useAlbums from "../context/albums/hooks/use-albums";
+import usePhoto from "../context/photos/hooks/use-photo";
+import type { Photo } from "../context/photos/models/photo";
+
+export default function PagePhotoDetails() {
+  const { id } = useParams();
+  const { photo, previousPhotoId, nextPhotoId, isLoadingPhoto } = usePhoto(id);
+  const { albums, isLoadingAlbums } = useAlbums();
+
+  if (!isLoadingPhoto && !photo) {
+    return <div>Foto não encontrada!</div>;
+  }
+
+  return (
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text as="h2" variant="heading-large">
+            {photo?.title}
+          </Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotoNavigator
+          previousPhotoId={previousPhotoId}
+          nextPhotoId={nextPhotoId}
+          loading={isLoadingPhoto}
+        />
+      </header>
+
+      <div className="grid grid-cols-[21rem_1fr] gap-24">
+        <div className=" space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`${import.meta.env.VITE_IMAGES_URL}/${photo?.imageId}`}
+              title={photo?.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10" />
+          )}
+        </div>
+
+        <div className="py-3">
+          <Text as="h3" variant="heading-medium" className="mb-6">
+            Álbuns
+          </Text>
+
+          <AlbumsListSelectable
+            photo={photo as Photo}
+            albums={albums}
+            loading={isLoadingAlbums}
+          />
+        </div>
+      </div>
+    </Container>
+  );
+}
