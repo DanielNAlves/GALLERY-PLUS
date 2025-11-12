41e91c341d4c9197ecc093d5a3cc120c82aa72cf
Daniel Alves <11daniel.neves.alves@gmail.com>
2025-11-11 23:40:49 -0300

    refactor(app): Atualiza App.tsx para integrar novos contextos

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import LayoutMain from "./pages/layout-main";
 import PageHome from "./pages/page-home";
 import PagePhotoDetails from "./pages/page-photo-details";
 import { NuqsAdapter } from "nuqs/adapters/react-router/v7";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NuqsAdapter>
+        <Toaster position="bottom-center" />
         <BrowserRouter>
           <Routes>
             <Route element={<LayoutMain />}>
